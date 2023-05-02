cb37f5ea8449456906b0157ccc619c0cdc825927
gasolina90 <moralerick90@gmail.com>
2023-05-02 14:34:30 -0600

    modified file

--- recog_digits.py
+++ recog_digits.py
@@ -5,3 +5,7 @@ from sklearn.model_selection import train_test_split
 # Load and save the sample dataset to a variable
 digits = datasets.load_digits()
 
+_, ax = plt.subplot(nrows=1, ncols=4, figsize=(10,3)) # Define the number of rows, columns, and size of the subplot 
+
+for ax, image, label in zip(ax, digits.images, digits.target):
+    pass
